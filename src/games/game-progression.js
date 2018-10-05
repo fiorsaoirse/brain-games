5ba1c3ad2141c31a458f927148351fb91c45a760
fiorsaoirse <solovyanovamaria@yandex.ru>
2018-10-05 12:16:52 +0300

    change generate progression logic

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -7,23 +7,27 @@ import generateNum from '../generatorNum';
 const description = 'What number is missing in this progression?';
 const countOfElements = 10;
 
-const generateGameData = () => {
-  // Генерация шага для прогрессии
-  const step = generateNum(1, 10);
-  const firstNumber = generateNum(1, 100);
-  const arrayOfNumbers = [firstNumber];
+const genereteProgression = (firstElem, step) => {
+  const arrayOfNumbers = [firstElem];
   for (let i = 0; i < countOfElements - 1; i += 1) {
     const newElement = arrayOfNumbers[i] + step;
     arrayOfNumbers.push(newElement);
   }
+  return arrayOfNumbers;
+};
 
+const generateGameData = () => {
+  // Генерация шага для прогрессии
+  const step = generateNum(1, 10);
+  const firstNumber = generateNum(1, 100);
+  const progression = genereteProgression(firstNumber, step);
   // Получаем случайный индекс из массива, элемент с этим индексом "спрячем"
-  const hiddenElemIndex = Math.floor(Math.random() * arrayOfNumbers.length);
-  const rightAnswer = arrayOfNumbers[hiddenElemIndex];
+  const hiddenElemIndex = Math.floor(Math.random() * progression.length);
+  const rightAnswer = progression[hiddenElemIndex];
 
   // "Прячем" элемент
-  arrayOfNumbers[hiddenElemIndex] = '...';
-  const question = arrayOfNumbers.join(' ');
+  progression[hiddenElemIndex] = '...';
+  const question = progression.join(' ');
   return cons(question, rightAnswer);
 };
 
